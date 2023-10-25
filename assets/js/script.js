185316a207bb0362cec6ac93fdc43569af65c3cd
Sanya Sharma <sanyaasharma3214@gmail.com>
2023-10-25 20:21:27 +0530

    About Section Modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,33 +13,33 @@ menu.onclick = () => {
 
 const addEventOnElem = function (elem, type, callback) {
     if (elem.length > 1) {
-      for (let i = 0; i < elem.length; i++) {
-        elem[i].addEventListener(type, callback);
-      }
+        for (let i = 0; i < elem.length; i++) {
+            elem[i].addEventListener(type, callback);
+        }
     } else {
-      elem.addEventListener(type, callback);
+        elem.addEventListener(type, callback);
     }
-  }
-  /**
- * header & back top btn active when window scroll down to 100px
- */
+}
+/**
+* header & back top btn active when window scroll down to 100px
+*/
 
 const header = document.querySelector(".header");
 //const backTopBtn = document.querySelector("[data-back-top-btn]");
 
 const showElemOnScroll = function () {
-  if (window.scrollY > 100) {
-    header.classList.add("active");
-  } else {
-    header.classList.remove("active");
-  }
+    if (window.scrollY > 100) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
 }
 
 addEventOnElem(window, "scroll", showElemOnScroll);
 const heroo = document.querySelector('.hero-list');
-function renderHero(){
-  hero.forEach((item) => {
-    heroo.innerHTML += `
+function renderHero() {
+    hero.forEach((item) => {
+        heroo.innerHTML += `
     <li class="${item.itsClass}">
     <div class="hero-card">
 
@@ -56,6 +56,6 @@ function renderHero(){
 
     </div>
   </li>`;
-  });
+    });
 }
 renderHero();
