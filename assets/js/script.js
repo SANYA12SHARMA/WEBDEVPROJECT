d9f6230a1b11afe26e1b052cdc9c3d0c423f0f95
Sanya Sharma <sanyaasharma3214@gmail.com>
2023-10-29 16:23:37 +0530

    Local Storage with JSON

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,7 @@ menu.onclick = () => {
     menu.classList.toggle('move');
 }
 
-/**
- * add event on element
- */
+/* add event on element */
 
 const addEventOnElem = function (elem, type, callback) {
     if (elem.length > 1) {
@@ -20,18 +18,18 @@ const addEventOnElem = function (elem, type, callback) {
         elem.addEventListener(type, callback);
     }
 }
-/**
-* header & back top btn active when window scroll down to 100px
-*/
+/* header & back top btn active when window scroll down to 100px */
 
 const header = document.querySelector(".header");
-//const backTopBtn = document.querySelector("[data-back-top-btn]");
+const backTopBtn = document.querySelector(".back-top-btn");
 
 const showElemOnScroll = function () {
     if (window.scrollY > 100) {
         header.classList.add("active");
+        backTopBtn.classList.add("active");
     } else {
         header.classList.remove("active");
+        backTopBtn.classList.remove("active");
     }
 }
 
@@ -59,43 +57,92 @@ function renderHero() {
     });
 }
 renderHero();
-const product = document.querySelector('.product-list');
-function renderProducts() {
-    products.forEach((item) => {
-        product.innerHTML += `
-    <li class="${item.itembelong}">
-    <div class="product-card">
-      <a href="#" class="card-banner img-holder has-before" style="--width: 300; --height: 300;">
-        <img src="${item.imgSrc}" width="300" height="300" loading="lazy"
-          alt="Artificial potted plant" class="img-cover">
-        <ul class="card-action-list">
-          <li>
-            <button class="card-action-btn" aria-label="add to cart" title="add to cart">
-              <ion-icon name="bag-handle-outline" aria-hidden="true"></ion-icon>
-            </button>
-          </li>
+const ProductsDOM = document.querySelector(".product-list");
+//initialise cart(getting info from local storage)
+let cart = [];
+//getting Products 
+class Products{
+    async getProducts(){
+        try{
+            let result = await fetch('products.json');
+            let data = await result.json();
+            let products = data.items;
+            products = products.map(item => {
+                const {itembelong,name,price} = item.fields;
+                const {id} = item.sys;
+                const image = item.fields.image.fields.file.url;
+                return {itembelong,name,price,id,image};
+            });
+            return products;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+};
 
-          <li>
-            <button class="card-action-btn" aria-label="add to wishlist" title="add to wishlist">
-              <ion-icon name="heart-outline" aria-hidden="true"></ion-icon>
-            </button>
+//Display Products
+class UI{
+    displayProducts(products){
+        let result = '';
+        products.forEach(item =>{
+            result+=`
+            <li class="${item.itembelong}">
+            <div class="product-card">
+              <a href="#" class="card-banner img-holder has-before" style="--width: 300; --height: 300;">
+                <img src="${item.image}" width="300" height="300" loading="lazy"
+                  alt="Artificial potted plant" class="img-cover">
+                <ul class="card-action-list">
+                  <li>
+                    <button class="card-action-btn" aria-label="add to cart" title="add to cart"  data-id="${item.id}">
+                      <ion-icon name="bag-handle-outline" aria-hidden="true"></ion-icon>
+                    </button>
+                  </li>
+                </ul>
+              </a>
+              <div class="card-content">
+                <h3 class="h3">
+                  <a href="#" class="card-title">${item.name}</a>
+                </h3>
+                <div class="card-price">
+                  <data class="price" value="40">$${item.price}</data>
+                </div>
+              </div>
+            </div>
           </li>
-        </ul>
-      </a>
-      <div class="card-content">
-        <h3 class="h3">
-          <a href="#" class="card-title">${item.name}</a>
-        </h3>
-        <div class="card-price">
-          <data class="price" value="40">$${item.price}</data>
-        </div>
-      </div>
-    </div>
-  </li>
-    `;
-    });
+            `;
+        });
+        ProductsDOM.innerHTML = result;
+    }
+    getBagButtons(){
+      const btns= [...document.querySelectorAll(".card-action-btn")];
+      console.log(btns);
+    }
+};
+
+//Local Storage
+class Storage{
+  static saveProducts(products){
+    localStorage.setItem("products",JSON.stringify(products));
 }
-renderProducts();
+};
+
+document.addEventListener("DOMContentLoaded",()=>{
+    const ui= new UI();
+    const products = new Products();
+
+    //get all products
+    products.getProducts().then(products => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);//static method
+    }).then(()=>{
+      ui.getBagButtons();
+    });
+    
+});
+
+
+
 /**
  * product filter
  */
